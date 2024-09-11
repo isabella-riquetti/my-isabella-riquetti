e52133ce31d4ddc67a132c3f01947e866011b8a7
Isabella <isabella@sayprimer.com>
2024-09-11 16:15:39 -0300

    New Identity

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,8 +9,8 @@ function Header() {
   return (
     <div>
       <div className='w-full h-20 py-2 flex gap-4 items-center'>
-        <div className="flex gap-2 items-center">
-          <Icon className="max-h-12 max-w-12" /><span>Riquetti</span>
+        <div className="flex gap-3 items-center">
+          <Icon className="max-h-12 max-w-12" /><span className="text-3xl font-semibold"><em className="text-4xl opacity-75">R</em><span className="text-purple-800">iquetti</span></span>
         </div>
         <div className="relative ml-auto flex gap-8 text-lg font-bold">
           <div className="absolute top-[-105px] left-[-105px] z-0 w-36 h-36 bg-purple-600 rounded-full"></div>
